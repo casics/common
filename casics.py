e608fbc23094db670042fb3efc770cc09b829812
Michael Hucka <mhucka@caltech.edu>
2017-11-05 12:46:24 -0800

    Add utilities for MongoDB documents for repo entries

--- casics.py
+++ casics.py
@@ -2,12 +2,44 @@
 '''
 casics: utilities for common CASICS-specific operations
 '''
-
 __version__ = '1.0.0'
 __author__  = 'Michael Hucka <mhucka@caltech.edu>'
 __email__   = 'mhucka@caltech.edu'
 __license__ = 'GPLv3'
 
+import os
+import sys
+
+
+# Utility functions for dealing with repo entries in our MongoDB database.
+# -----------------------------------------------------------------------------
+
+def e_path(entry):
+    '''Given an entry, return a full path of the form "owner/repo-name".'''
+    return entry['owner'] + '/' + entry['name']
+
+
+def e_summary(entry):
+    '''Summarize the full path and id number of the given entry.'''
+    summary = '{} (#{})'.format(e_path(entry), entry['_id'])
+    return summary.encode(sys.stdout.encoding, errors='replace').decode('ascii')
+
+
+def e_languages(entry):
+    '''Return the list of languages as a plain list of strings.'''
+    if not entry['languages']:
+        return []
+    elif entry['languages'] == -1:
+        return -1
+    elif isinstance(entry['languages'], list):
+        return [lang['name'] for lang in entry['languages']]
+    else:
+        # This shouldn't happen.
+        return entry['languages']
+
+
+# Other utilities to encapsulate common operations.
+# .............................................................................
 
 def generate_path(root, repo_id):
     '''Creates a path of the following form:
