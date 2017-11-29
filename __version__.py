a9f0262d40af1b7b3e69ffd82dc47507092a8ea6
Michael Hucka <mhucka@caltech.edu>
2017-11-29 13:38:21 -0800

    Add __version__.py

---
+++ __version__.py
@@ -0,0 +1,15 @@
+# =============================================================================
+# @file    __version__.py
+# @brief   CASICS common submodule version info
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/casics/common
+# =============================================================================
+
+__version__   = '1.0.0'
+__title__     = 'CASICS common code submodule'
+__url__       = 'https://github.com/casics/common'
+__author__    = 'Michael Hucka <mhucka@caltech.edu>'
+__email__     = 'casics-team@googlegroups.com'
+__license__   = 'GNU General Public License, version 3.0'
+__copyright__ = 'Copyright (C) 2015-2017 by the California Institute of Technology'
